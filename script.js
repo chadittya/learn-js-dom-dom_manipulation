152511510ffceb355274071454441ea6dff013f2
chadittya <chadittya@gmail.com>
2022-11-09 16:44:39 +0700

    commit pertama belajar JD materi dom manipulation

---
+++ script.js
@@ -0,0 +1,24 @@
+// 1. element innerHTML
+// const judul = document.getElementById("judul");
+// judul.innerHTML = "<em>Galih Arizza</em>";
+
+// const sectionA = document.querySelector("section#a");
+// sectionA.innerHTML = "hello world";
+
+// 2. element Style
+// const judul = document.querySelector("h1#judul");
+// judul.style.color = "lightblue";
+// judul.style.backgroundColor = "salmon";
+
+// 3. getAttribute, setAttribute, removeAttribute
+// const judul = document.getElementsByTagName("h1")[0];
+// judul.setAttribute("name", "galih");
+
+// const a = document.querySelector("section#a a");
+// a.setAttribute("id", "link");
+
+// 4. claslist
+const p2 = document.querySelector(".p2");
+p2.classList.add("label");
+p2.classList.remove("label");
+p2.classList.toggle("label");
